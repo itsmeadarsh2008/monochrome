455924cd7e3756ccfdc8218c443a87c158643c10
jacobyz211 <cyrusna29@icloud.com>
2026-05-23 02:21:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ try { return await c.req.json(); } catch(e) { return {}; }
 }
 
 const HIFI_INSTANCES = [
+'https://hifi-api-bffw.onrender.com',
 'https://hifi-api-workers.anothermoumen4.workers.dev',
 'https://hifi-api-pj08.onrender.com',
 'https://mono.kennyy.com.br/hifi-api',
