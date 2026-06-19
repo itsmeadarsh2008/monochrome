a9ae05fdc8eeac956af87667cdef6ad59d5e70f5
jacobyz211 <cyrusna29@icloud.com>
2026-06-19 11:18:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2091,6 +2091,7 @@ const EXTRA_SPINE_SOURCES = [
   'https://all-in-one.rickyaddons.dpdns.org/8spine-source.json',
   'https://eclipse3.cyrusna29.workers.dev/8spine-source.json',
   'https://qobuz-tidal-eclipse.cyrusna29.workers.dev/8spine-source.json',
+  'https://improved-all-in-one.cyrusna29.workers.dev/8spine-source.json',
 ];
 
 app.get('/8spine-source.json', async c => {
