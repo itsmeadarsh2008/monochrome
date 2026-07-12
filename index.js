f8786f7cc902bfa925ba786413fa4693beba9800
jacobyz211 <cyrusna29@icloud.com>
2026-07-12 12:49:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ try { return await c.req.json(); } catch(e) { return {}; }
 }
 
 const HIFI_INSTANCES = [
+'tidal-come-one.francescorick99.workers.dev',
 'https://hifi-api1.onrender.com',
 'https://hifi-api.thevolecitor.qzz.io/',
 'https://hifi-api-bffw.onrender.com',
